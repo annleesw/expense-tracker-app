9d06e5e2a585edc476886c3cf32c433f363f9eca
Ann Lee <ann@meloq.se>
2025-01-31 15:40:17 +0100

    modal sheet added

--- expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker_app/lib/widgets/expenses_list/expenses_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/widgets/expenses_list/expenses_item.dart';
 
 class ExpensesList extends StatelessWidget{
   const ExpensesList({
@@ -13,7 +14,7 @@ class ExpensesList extends StatelessWidget{
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length, 
-      itemBuilder: (ctx, index) => Text(expenses[index].title), // => same as return
+      itemBuilder: (ctx, index) => ExpenseItem(expenses[index]), // => same as return
       ); 
   }
 }
